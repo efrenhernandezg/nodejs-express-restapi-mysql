0808029545a03ee0040f44ef070ce83dbac7ff4c
efrenhernandezg <127789055+efrenhernandezg@users.noreply.github.com>
2023-03-22 18:31:13 -0400

    Adding the database code

--- db/database.sql
+++ db/database.sql
@@ -5,17 +5,19 @@ USE companydb;
 CREATE TABLE employee (
     id INT(11) NOT NULL AUTO_INCREMENT,
     name VARCHAR(45) DEFAULT NULL,
+    lastName VARCHAR(45) DEFAULT NULL,
+    country VARCHAR(45) DEFAULT NULL,
     salary INT(5) DEFAULT NULL,
     PRIMARY KEY (id)
 );
 
 DESCRIBE employee;
 
-INSERT INTO employee VALUES
-    (1, 'Efren', 2500),
-    (2, 'Andrea', 1500),
-    (3, 'Antonio', 2000),
-    (4, 'Cristina', 2500);
+INSERT INTO employees VALUES
+    (1, 'Efren', 'Hernandez', 'Venezuela', 2500),
+    (2, 'Andrea', 'Garcia', 'Argentina', 1500),
+    (3, 'Antonio', 'Guevara', 'Mexico', 2000),
+    (4, 'Cristina', 'Fernandez', 'Colombia', 2500);
 
 SELECT * FROM employee WHERE id = 1;
 DELETE FROM employye WHERE id = 5;
