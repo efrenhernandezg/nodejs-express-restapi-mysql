b371b94f748c018092ba20b30c3de82d6148fe21
efrenhernandezg <127789055+efrenhernandezg@users.noreply.github.com>
2023-03-24 13:08:26 -0400

    ready

--- src/config.js
+++ src/config.js
@@ -4,8 +4,8 @@ config();
 
 export const PORT = process.env.PORT || 3000;
 
-export const DB_HOST = process.env.HOST || '127.0.0.1';
-export const DB_USER = process.env.USER || 'root';
-export const DB_PASSWORD = process.env.PASSWORD || 'Eh15865685$';
-export const DB_DATABASE = process.env.DB_DATABASE || 'companydb';
-export const DB_PORT = process.env.DB_PORT || 3306;
+export const DB_HOST = process.env.DB_HOST;
+export const DB_USER = process.env.DB_USER;
+export const DB_PASSWORD = process.env.DB_PASSWORD;
+export const DB_DATABASE = process.env.DB_DATABASE;
+export const DB_PORT = process.env.DB_PORT;
